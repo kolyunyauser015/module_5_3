a60ee8f1616a78aef2add0b4a8c14aa0a6f814f8
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-09-03 19:44:45 +0600

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,58 @@
+class House:
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        return self.number_of_floors + value
+
+    def __radd__(self, value):
+        return self.number_of_floors + value
+
+    def __iadd__(self, value):
+        return self.number_of_floors + value
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+print(h1 == h2)
+h1.number_of_floors = h1.number_of_floors + 10
+print(h1)
+print(h1 == h2)
+h1.number_of_floors += 10
+print(h1)
+h2.number_of_floors = 10 + h2.number_of_floors
+print(h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 != h2)
